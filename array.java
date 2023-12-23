a3619e7f65d2edc0f3a1e4948fc3a02cecc8ac26
Xinyao Yin <xinyaoyin@xinyaos-mbp-2.home>
2023-12-22 19:49:53 -0500

    array is here

---
+++ array.java
@@ -0,0 +1,8 @@
+public class array{
+    public static void main(String args[]){
+        int nums[][] = new int[4][4];
+        for (int x=0; x<nums.length; x++){
+            System.out.println(nums[x]);
+        }
+    }
+}
